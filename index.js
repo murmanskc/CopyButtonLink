091faca68e6fabb1960b4e2a6ebe7045ceed0f9c
murmanskc <jonathanbong11@gmail.com>
2026-07-21 09:41:05 +0700

    Rewrite

--- index.js
+++ index.js
@@ -1,42 +1,53 @@
-import { patcher, metro } from "@vendetta";
-import { showToast } from "@vendetta/ui/toasts";
-import { clipboard } from "@vendetta/metro/common";
+(() => {
+  const v = window.vendetta || window.bunny || {};
+  const patcher = v.patcher;
+  const metro = v.metro;
+  const showToast = v.ui?.toasts?.showToast || console.log;
+  const clipboard = v.metro?.common?.clipboard;
 
-let unpatches = [];
+  let unpatches = [];
 
-export default {
-  onLoad: () => {
-    const ButtonModule = metro.findByProps("Button", "ButtonColors") || metro.findByName("Button");
-
-    if (!ButtonModule) {
-      showToast("Copy Button Link: Component not found");
-      return;
-    }
-
-    const unpatch = patcher.after("default", ButtonModule, (args, res) => {
-      const props = args[0];
-      const targetUrl = props?.url || props?.component?.url;
-
-      if (targetUrl && res?.props) {
-        const originalLongPress = res.props.onLongPress;
-
-        res.props.onLongPress = (e) => {
-          if (typeof originalLongPress === "function") {
-            originalLongPress(e);
-          }
-          clipboard.setString(targetUrl);
-          showToast("Copied button link!");
-        };
+  return {
+    onLoad: () => {
+      if (!metro || !patcher) {
+        showToast("Error: Mod APIs not accessible.");
+        return;
       }
-    });
 
-    unpatches.push(unpatch);
-  },
+      const ButtonModule = metro.findByProps("Button", "ButtonColors") || metro.findByName("Button");
 
-  onUnload: () => {
-    for (const unpatch of unpatches) {
-      unpatch();
+      if (!ButtonModule) {
+        showToast("Copy Button Link: Button component not found.");
+        return;
+      }
+
+      const unpatch = patcher.after("default", ButtonModule, (args, res) => {
+        const props = args[0];
+        const targetUrl = props?.url || props?.component?.url;
+
+        if (targetUrl && res?.props) {
+          const originalLongPress = res.props.onLongPress;
+
+          res.props.onLongPress = (e) => {
+            if (typeof originalLongPress === "function") {
+              originalLongPress(e);
+            }
+            if (clipboard?.setString) {
+              clipboard.setString(targetUrl);
+            }
+            showToast("Copied button link!");
+          };
+        }
+      });
+
+      unpatches.push(unpatch);
+    },
+
+    onUnload: () => {
+      for (const unpatch of unpatches) {
+        if (typeof unpatch === "function") unpatch();
+      }
+      unpatches = [];
     }
-    unpatches = [];
-  }
-};
+  };
+})();
