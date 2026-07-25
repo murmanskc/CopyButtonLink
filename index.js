94a0c981fe68fdd30be9faf103b6fa5999bcd6e6
murmanskc <jonathanbong11@gmail.com>
2026-07-25 12:46:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,68 @@
-var CopyButtonLink = (function () {
-  'use strict';
-
-  var patcher = vendetta.patcher;
-  var metro = vendetta.metro;
-  var toasts = vendetta.ui.toasts;
-  var common = vendetta.metro.common;
-
-  var unpatches = [];
+(() => {
+  let unpatches = [];
 
   return {
-    onLoad: function () {
-      var ButtonModule = metro.findByProps("Button", "ButtonColors") || metro.findByName("Button");
+    onLoad: () => {
+      try {
+        // Safe global mod API lookup
+        const v = window.vendetta || window.bunny || window.kettu || (typeof vendetta !== "undefined" ? vendetta : null);
+        if (!v) return;
 
-      if (!ButtonModule) {
-        if (toasts && toasts.showToast) toasts.showToast("Copy Button Link: Component not found");
-        return;
-      }
+        const patcher = v.patcher;
+        const metro = v.metro;
+        const toasts = v.ui?.toasts;
+        const clipboard = v.metro?.common?.clipboard;
 
-      var unpatch = patcher.after("default", ButtonModule, function (args, res) {
-        var props = args[0];
-        var targetUrl = props && (props.url || (props.component && props.component.url));
+        if (!patcher || !metro) return;
 
-        if (targetUrl && res && res.props) {
-          var originalLongPress = res.props.onLongPress;
+        // Search for Discord's Button module with multiple fallbacks
+        const ButtonModule = metro.findByProps("Button", "ButtonColors") || 
+                             metro.findByName("Button", false) || 
+                             metro.findByProps("ButtonColors");
 
-          res.props.onLongPress = function (e) {
-            if (typeof originalLongPress === "function") {
-              originalLongPress(e);
-            }
-            if (common && common.clipboard && common.clipboard.setString) {
-              common.clipboard.setString(targetUrl);
-            }
-            if (toasts && toasts.showToast) {
-              toasts.showToast("Copied button link!");
-            }
-          };
+        if (!ButtonModule) {
+          if (toasts?.showToast) toasts.showToast("Copy Button Link: Button module not found.");
+          return;
         }
-      });
 
-      unpatches.push(unpatch);
+        const patchMethod = ButtonModule.default ? "default" : "render";
+
+        const unpatch = patcher.after(patchMethod, ButtonModule, (args, res) => {
+          try {
+            const props = args[0];
+            const targetUrl = props?.url || props?.component?.url;
+
+            if (targetUrl && res?.props) {
+              const originalLongPress = res.props.onLongPress;
+
+              res.props.onLongPress = (e) => {
+                if (typeof originalLongPress === "function") {
+                  originalLongPress(e);
+                }
+                if (clipboard?.setString) {
+                  clipboard.setString(targetUrl);
+                }
+                if (toasts?.showToast) {
+                  toasts.showToast("Copied button link!");
+                }
+              };
+            }
+          } catch (patchErr) {
+            console.error("[CopyButtonLink] Patch error:", patchErr);
+          }
+        });
+
+        if (unpatch) unpatches.push(unpatch);
+      } catch (err) {
+        console.error("[CopyButtonLink] onLoad error:", err);
+      }
     },
 
-    onUnload: function () {
-      for (var i = 0; i < unpatches.length; i++) {
-        if (typeof unpatches[i] === "function") unpatches[i]();
+    onUnload: () => {
+      for (const unpatch of unpatches) {
+        if (typeof unpatch === "function") {
+          try { unpatch(); } catch (_) {}
+        }
       }
       unpatches = [];
     }
